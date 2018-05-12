025c81187701da73399e664452e20cb400f69e7a
Zisko22 <zisko22@gmail.com>
2018-05-11 18:51:12 -0700

    hopeful dll fix

---
+++ TwentyOne/ExceptionEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwentyOne
+{
+    public class ExceptionEntity
+    {
+		public int Id { get; set; }
+		public string ExceptionType { get; set; }
+		public string ExceptionMessage { get; set; }
+		public DateTime TimeStamp { get; set; }
+    }
+}
